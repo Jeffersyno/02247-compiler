25bd76b4058dd8e308158a86708c79574dda5aa7
Andres Cecilia <a.cecilia.luque@gmail.com>
2017-03-19 18:27:54 +0100

    Reorganized code. Removed PointerStatusMap.cpp: now all the definitions are in the headers file. This allows less boilerplate code and avoids linking problems (symbol not found exception). This fixes the symbol not found error we were getting in macOS

--- nullderef/PointerStatusMap.h
+++ nullderef/PointerStatusMap.h
@@ -5,34 +5,34 @@
 #include <functional>
 #include <unordered_map>
 #include <llvm/IR/Value.h>
+#include <llvm/ADT/Hashing.h>
+
+// TODO: unused class, remove? This is maybe some trush form initial development?
+namespace std {
+    template<> struct hash<PointerKey> {
+        size_t operator()(PointerKey const& key) const noexcept { return key.hash(); }
+    };
+}
 
 class PointerKey {
 public:
-    bool operator ==(const PointerKey other) const;
-    virtual std::size_t hash() const noexcept;
+    bool operator ==(const PointerKey other) const { return this->hash() == other.hash(); }
+    virtual std::size_t hash() const noexcept { return llvm::hash_value(0); }
 };
 
-class LLVMValueKey : public PointerKey {
+class LLVMValueKey: public PointerKey {
     const llvm::Value* value;
 
 public:
-    std::size_t hash() const noexcept override;
+    std::size_t hash() const noexcept override { return llvm::hash_value(this->value); }
 };
 
 class StructFieldKey : public PointerKey {
     llvm::Value* strct;
     int field_no;
 public:
-    std::size_t hash() const noexcept override;
+    std::size_t hash() const noexcept override { return (31 + llvm::hash_value(this->strct)) * 23 + (this->field_no << 2); }
 };
-
-namespace std {
-    template<> struct hash<PointerKey> {
-        std::size_t operator()(PointerKey const& key) const noexcept {
-            return key.hash();
-        }
-    };
-}
 
 class PointerStatus {
 public:
@@ -40,9 +40,9 @@ public:
     static const short NON_NIL = 2;
     static const short DONT_KNOW = 1 | 2;
 
-    virtual short status() const;
-    virtual int depth() const;
-    virtual bool isNullDeref() const;
+    virtual short status() const { return 0; }
+    virtual int depth() const { return 0; }
+    virtual bool isNullDeref() const { return true; }
 };
 
 class PureValue : public PointerStatus {
@@ -52,8 +52,8 @@ public:
     PureValue();
     PureValue(short status);
 
-    short status() const override;
-    int depth() const override;
+    short status() const override { return this->status_value; }
+    int depth() const override { return 0; }
 };
 
 class ImitationValue : public PointerStatus {
@@ -62,8 +62,8 @@ class ImitationValue : public PointerStatus {
 public:
     ImitationValue(const PointerStatus& imitated);
 
-    short status() const override;
-    int depth() const override;
+    short status() const override { return this->imitated.status(); }
+    int depth() const override { return this->imitated.depth(); }
 };
 
 class ReferenceValue : public PointerStatus {
@@ -72,18 +72,17 @@ class ReferenceValue : public PointerStatus {
 public:
     ReferenceValue(const PointerStatus& ref);
 
-    short status() const override;
-    int depth() const override;
+    short status() const override { return this->ref.status(); }
+    int depth() const override { return 1 + this->ref.depth(); }
 };
 
 class TrackerMap {
     std::unordered_map<PointerKey, PointerStatus> map;
 public:
-    /* We pass keys and statusses as value, make sure our types don't grown too large */
-    PointerStatus get(PointerKey key);
-    bool contains(PointerKey key);
-    void put(PointerKey key, PointerStatus value);
+    /* We pass keys and statuses as value, make sure our types don't grown too large */
+    PointerStatus get(PointerKey key) { return this->map[key]; }
+    bool contains(PointerKey key) { return this->map.count(key); }
+    void put(PointerKey key, PointerStatus value) { this->map[key] = value; }
 };
-
 
 #endif // POINTER_STATUS_MAP_H
