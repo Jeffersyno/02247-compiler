0b7c40713e763057bbdefd26ae907b898b6dc832
Laurens <laurens.devos@outlook.com>
2017-05-04 09:34:42 +0200

    Additional example + graph dump update

--- examples/dereference.c
+++ examples/dereference.c
@@ -1,10 +1,6 @@
-#include <stdio.h>
-
+struct A { int b; int *ptr; };
 int main() {
-	int number = 5;
-	int* ptr1 = &number;
-	printf("Pointer address = %p\n", (void *) ptr1); //0x7fff54d4ab0c
-	printf("Now we make the pointer null\n");
-	ptr1 = 0;
-	printf("Pointer address = %p\n", (void *) ptr1); //0x00
+    struct A a;
+    a.ptr = 0;
+    int val = *(a.ptr);
 }
